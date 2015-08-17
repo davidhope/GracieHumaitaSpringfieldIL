a0c1d92d9277c6f1bea49f80bf5522517de6f515
David Hope <flav.flavor@gmail.com>
2015-08-17 18:22:57 -0500

    bootstrapping angular

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
-var app = angular.module('gracie',[]);
+//var app = angular.module('gracie',[]);
 
-
+// one file
+// NOTE: the immediately invoked function expression 
+// is used to exemplify different files and is not required
+(function(){
+   // declaring the module in one file / anonymous function
+   // (only pass a second parameter THIS ONE TIME as a redecleration creates bugs
+   // which are very hard to dedect)
+   angular.module('gracie',['ngRoute'])
+    .config(function($routeProvider){
+    $routeProvider
+    	.when('/instructors',
+    		{
+    			templateUrl: '/gracie/templates/pages/instructors/index.html',
+				controller: 'MainController',
+				controllerAs: 'ctrl'
+    		})
+    	.when('/classes',
+    		{
+    			templateUrl: '/gracie/templates/pages/classes/index.html',
+				controller: 'MainController',
+				controllerAs: 'ctrl'
+			})
+    	.when('/',
+    		{	
+    			templateUrl: '/gracie/templates/pages/main.html',
+				controller: 'MainController',
+				controllerAs: 'ctrl'
+			})
+    	.otherwise({redirectTo: '/'})
+  });             
+})();
